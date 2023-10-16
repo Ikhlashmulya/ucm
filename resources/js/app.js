64e100c219b29dada1ea68db85cf63cb4e337191
Muhammad Muslim Abdul Jabbaar <mmuslimabdulj@gmail.com>
2023-10-16 20:59:35 +0700

    fix collapse toggle not work (hamburger menu) in mobile version

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,18 @@
 import "./bootstrap";
 import "flowbite";
 import { darkMode } from "./flowbite-plugin";
-import { initAccordions, initModals } from "flowbite";
+import { initAccordions, initCollapses, initModals } from "flowbite";
 
 document.addEventListener("livewire:navigated", () => {
     darkMode();
+    initCollapses();
     initModals(); // Modal in beranda
     initAccordions(); // Accordion in Tentang
 });
 
 document.addEventListener("DOMContentLoaded", () => {
     darkMode();
+    initCollapses();
     initModals();
     initAccordions();
 });
